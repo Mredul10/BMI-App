262b0e9f119a57b494d8c0c7ce5c51bb059f3f71
Mredul10 <mredul110@gmail.com>
2024-11-22 21:27:54 +0600

    Calculate BMI

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -12,66 +12,125 @@ class _MyHomePageState extends State<MyHomePage> {
   var ftController = TextEditingController();
   var inController = TextEditingController();
 
+  var result = "";
+
+ 
+
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your BMI'),
+        title: Text('Your BMI',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.indigo,
       ),
-      body: Center(
-        child: Container(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Bmi', style: TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.w700,
-              ),),
-          
-              SizedBox(height: 20,),
-          
-          
-              TextField(
-                controller: wtController,
-                decoration: InputDecoration(
-                label: Text('Enter Your Weight (in KG)'),
-                prefixIcon: Icon(Icons.line_weight),
+      body: Container(
+        color: Colors.amber,
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('BMI', style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.w700,
+                ),),
+            
+                SizedBox(height: 20,),
+            
+            
+                TextField(
+                  controller: wtController,
+                  decoration: InputDecoration(
+                  label: Text('Enter Your Weight (in KG)',style: TextStyle(fontSize: 20),),
+                  prefixIcon: Icon(Icons.line_weight),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(height: 10,),
-          
-              TextField(
-                controller: ftController,
-                decoration: InputDecoration(
-                label: Text('Enter Your Height (in Feet)'),
-                prefixIcon: Icon(Icons.height),
+                SizedBox(height: 10,),
+            
+                TextField(
+                  controller: ftController,
+                  decoration: InputDecoration(
+                  label: Text('Enter Your Height (in Feet)',style: TextStyle(fontSize: 20),),
+                  prefixIcon: Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(height: 10,),
-          
-              TextField(
-                controller: inController,
-                decoration: InputDecoration(
-                label: Text('Enter Your Height (in Inch)'),
-                prefixIcon: Icon(Icons.height),
+                SizedBox(height: 10,),
+            
+                TextField(
+                  controller: inController,
+                  decoration: InputDecoration(
+                  label: Text('Enter Your Height (in Inch)',style: TextStyle(fontSize: 20),),
+                  prefixIcon: Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
-              ),
+        
+                SizedBox(height: 20,),
+        
+                ElevatedButton(onPressed: (){
+        
+                  var wt = wtController.text.toString();
+                  var ft = ftController.text.toString();
+                  var inch = inController.text.toString();
+        
+                  if(wt!="" && ft!="" && inch!=""){
+                    //BMI Calculation
+        
+                    var iwt = int.parse(wt);
+                    var ift = int.parse(ft);
+                    var iinch = int.parse(inch);
+        
+                    var tInch = (ift*12) + iinch;
+        
+                    var tCm = tInch*2.54;
+                    var tM = tCm/100;
+        
+                    var bmi = iwt/(tM*tM);
 
-              SizedBox(height: 20,),
+                   var msg = "";
+                  //  var bgColour = "";
 
-              ElevatedButton(onPressed: (){}, child: Text('Calculate'),),
+                    if(bmi>25){
+                      msg = "You are OverWeight";
+                      // bgColour = Colors.purple;
 
+                    } else if(bmi<18.5){
+                      msg = "You are UnderWeight";
+                      // bgColour = Colors.red;
 
-              Text(''),
-          
-          
-            ],
+                    }else{
+                      msg = "You are Healthy";
+                      // bgColour = Colors.green;
+
+                    }
+        
+                    setState(() {
+                      result = "$msg \n BMI is : ${bmi.toStringAsFixed(2)}";
+                    });
+        
+        
+                  } else{
+                    setState(() {
+                      result = "Please fill all the required blanks!!";
+                    });
+                  }
+        
+        
+                }, child: Text('Calculate'),),
+        
+                SizedBox(height: 10,),
+        
+        
+                Text(result, style: TextStyle(fontSize: 19),),
+            
+            
+              ],
+            ),
           ),
         ),
       ),
